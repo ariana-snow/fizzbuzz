b908757b5dee26551b095f5eb463b796066bea2f
Ariana Snow <79360832+ariana-snow@users.noreply.github.com>
2021-03-03 17:33:42 +0000

    first commit

---
+++ java.js
@@ -0,0 +1,29 @@
+//FizzBuzz
+for (let i = 0; i < 100; i++)
+{
+  if (i % 3 === 0 && i % 5 ===0){
+    console.log("Fizzbuzz");
+  }
+  else if (i % 3===0 ) {
+    console.log("Fizz");
+  }
+  else if (i % 5===0 ) {
+    console.log("Buzz");
+}
+  else { console.log(i); } 
+}
+
+//FizzBuzzCounter
+for (let i = 0; i < 100; i++; )
+{
+  if (i % 3 === 0 && i % 5 ===0){
+    console.log("Fizzbuzz");
+  }
+  else if (i % 3===0 ) {
+    console.log("Fizz");
+  }
+  else if (i % 5===0 ) {
+    console.log("Buzz");
+}
+  else { console.log(i); } 
+}
